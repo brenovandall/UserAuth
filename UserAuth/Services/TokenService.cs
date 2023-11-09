9985a0e2e7cf93d0061133981023dcd8b6988163
breno van dall <vbreno335@gmail.com>
2023-11-09 12:41:10 -0300

    datetime added on the token

--- UserAuth/Services/TokenService.cs
+++ UserAuth/Services/TokenService.cs
@@ -9,17 +9,18 @@ namespace UserAuth.Services;
 
 public class TokenService
 {
-    public string GenerateToken(User? user)
+    public string GenerateToken(User user)
     {
         Claim[] claims = new Claim[]
         {
             new Claim("id", user.Id),
-            new Claim("username", user.UserName)
+            new Claim("username", user.UserName),
+            new Claim("loginTimestamp", DateTime.UtcNow.ToString())
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("h527dndfajdf8924gnshfkav8bdjd"));
+        var keyS = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("DUHW92831DINDW0U28BD92DJS9J1"));
 
-        var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var signingCredentials = new SigningCredentials(keyS, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
                 expires: DateTime.Now.AddMinutes(10),
